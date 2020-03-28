a2fb84ec74245f82c6fe6e42e0590eaa3ead01ba
Thiago <thiagoworkspace@outlook.com>
2020-03-28 16:59:03 -0300

    .

--- Gamification/src/Components/HomePoints.js
+++ Gamification/src/Components/HomePoints.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text, TouchableOpacity } from 'react-native';
 import HomeStyle from "../Styles/HomeStyle";
 
 export default class HomePoints extends Component {
@@ -11,8 +11,280 @@ export default class HomePoints extends Component {
 
   render() {
     return (
-      <View style={HomeStyle.containerPrincipalHp}>
-        <Text> HomePoints </Text>
+      
+      <View style={HomeStyle.containerHomePoints}>
+        <View style={HomeStyle.containerHead}></View>
+
+      
+        <View style={HomeStyle.containerBodyHP}>
+          <Text style={HomeStyle.textTitle}> Escolha onde Aplicar os Pontos </Text>
+          
+          <ScrollView>
+          <View style={HomeStyle.ContainertextOptions}>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Mensalidade Mês Janeiro</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Mensalidade Mês Fevereiro</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Mensalidade Mês Março</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Mensalidade Mês Abril</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Mensalidade Mês Maio</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Mensalidade Mês Junho</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>HackFaesa 10.0</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>BootCamp</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Centro de Vivencia (Cantina1)</Text>
+          </TouchableOpacity>
+
+          
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Centro de Vivencia (Cantina2)</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Centro de Vivencia (Cantina3)</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Centro de Vivencia (Xerox)</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>NAT (Xerox)</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Local Aleatorio 1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Local Aleatorio 2</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Local Aleatorio 3</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Local Aleatorio 4</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Local Aleatorio 5</Text>
+          </TouchableOpacity>
+
+          
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto2</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto3</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto4</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto5</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto6</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto7</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto8</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto9</Text>
+          </TouchableOpacity>
+
+          
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto10</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto11</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto12</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto13</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto14</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto15</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto16</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto17</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto18</Text>
+          </TouchableOpacity>
+
+          
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto19</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto20</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto21</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto22</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto23</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto24</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto25</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto26</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto27</Text>
+          </TouchableOpacity>
+
+          
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto28</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto29</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto30</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto31</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto23</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style = {HomeStyle.customButton}>
+            <Text style={HomeStyle.text}>Texto1</Text>
+          </TouchableOpacity>
+
+          
+        </View>
+        </ScrollView>
+       
+        </View>
       </View>
     );
   }
--- Gamification/src/Styles/HomeStyle.js
+++ Gamification/src/Styles/HomeStyle.js
@@ -12,7 +12,7 @@ const HomeStyle = StyleSheet.create({
 
     containerHead: {
         flex: 1,
-       height: 50, backgroundColor: '#696969'
+        height: 50, backgroundColor: '#696969'
     },
 
     containerBody: {
@@ -25,20 +25,20 @@ const HomeStyle = StyleSheet.create({
         height: 100, backgroundColor: '#696969'
     },
 
-    LinhaView:{
+    LinhaView: {
         flex: 0.01,
         backgroundColor: 'white'
     },
-    textoInfoUser:{
+    textoInfoUser: {
         //flex: 2,
         marginRight: 180, fontSize: 12, margin: 2
     },
 
-    infoPontos:{
-       borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+    infoPontos: {
+        borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
         width: Dimensions.get('window').width * 0.5,
         height: Dimensions.get('window').width * 0.5,
-        backgroundColor:  '#00BFFF',
+        backgroundColor: '#00BFFF',
         justifyContent: 'center',
         alignItems: 'center'
     },
@@ -46,10 +46,52 @@ const HomeStyle = StyleSheet.create({
 
     //Estilos para a pagina HomePoints
 
-    containerPrincipalHp:{
-        flex:1,
-        backgroundColor: '#696969'
+    containerHomePoints: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch'
     },
+
+    text: {
+        color: 'white',
+        fontSize: 17,
+        textAlign: 'center',
+        //margin: 12
+
+    },
+
+        textTitle:{
+            color: 'white',
+            fontSize: 17,
+            textAlign: 'center'
+        },  
+
+    customButton: {
+
+        marginRight: 40,
+        marginLeft: 40,
+        marginTop: 10,
+        paddingTop: 20,
+        paddingBottom: 20,
+        backgroundColor: '#68a0cf',
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: '#0000FF'
+    },
+
+    containerBodyHP: {
+        flex: 7.5,
+        height: 50, backgroundColor: '#696969',
+    },
+
+    ContainertextOptions: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingVertical: 40,
+
+    }
 });
 
 export default HomeStyle;
+
