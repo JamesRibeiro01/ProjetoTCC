c32521838389098b825cfc5fc012368c29a807ef
Thiago <thiagoworkspace@outlook.com>
2020-04-20 18:49:46 -0300

    vector icon

--- GamificationApp/src/Routes/Routes.js
+++ GamificationApp/src/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Icon from "react-native-vector-icons/Ionicons";
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import AboutScreen from "../Components/AboutScreen";
@@ -9,6 +10,7 @@ import NextEvents from "../Components/NextEvents";
 import PointScreen from "../Components/PointScreen";
 import ProfileScreen from "../Components/ProfileScreen";
 import HomeScreen from "../Components/HomeScreen";
+
 
 const HomeStack = createStackNavigator();
 const ProfileStack = createStackNavigator();
@@ -22,15 +24,38 @@ const Drawer = createDrawerNavigator();
 //HOMESCREENSTACK
 const HomeStackScreen = ({ navigation }) => {
     return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
+        <HomeStack.Navigator screenOptions ={{
+            headerStyle:{
+                backgroundColor: '#009387',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold'
+            }
+        }}>
+            <HomeStack.Screen name="HomeScreen" component={HomeScreen} options = {{
+                title: 'OverViwe',
+                headerLeft: ()=>{
+                    <Icon.Button  size = {25} backgroundColor = "black" options={()=>
+                        navigation.openDrawer()
+                    }></Icon.Button>
+                }
+            }}/>
         </HomeStack.Navigator>
     );
 };
 
 const ProfileStackScreen = ({ navigation }) => {
     return (
-        <ProfileStack.Navigator>
+        <ProfileStack.Navigator screenOptions ={{
+            headerStyle:{
+                backgroundColor: '#009387',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold'
+            }
+        }}>
             <ProfileStack.Screen name="ProfileScreen" component={ProfileScreen} />
         </ProfileStack.Navigator>
     );
@@ -39,7 +64,15 @@ const ProfileStackScreen = ({ navigation }) => {
 
 const PointStackScreen = ({ navigation }) => {
     return (
-        <PointStack.Navigator>
+        <PointStack.Navigator screenOptions ={{
+            headerStyle:{
+                backgroundColor: '#009387',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold'
+            }
+        }}>
             <PointStack.Screen name="PointScreen" component={PointScreen} />
         </PointStack.Navigator>
     );
@@ -48,7 +81,15 @@ const PointStackScreen = ({ navigation }) => {
 
 const NextEventStackScreen = ({ navigation }) => {
     return (
-        <NextEventStack.Navigator>
+        <NextEventStack.Navigator screenOptions ={{
+            headerStyle:{
+                backgroundColor: '#009387',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold'
+            }
+        }}>
             <NextEventStack.Screen name="NextEvents" component={NextEvents} />
         </NextEventStack.Navigator>
     );
@@ -57,7 +98,15 @@ const NextEventStackScreen = ({ navigation }) => {
 
 const EventStackScreen = ({ navigation }) => {
     return (
-        <EventStack.Navigator>
+        <EventStack.Navigator screenOptions ={{
+            headerStyle:{
+                backgroundColor: '#009387',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold'
+            }
+        }}>
         <EventStack.Screen name="EventScreen" component={EventScreen} />
     </EventStack.Navigator>
     );
@@ -66,7 +115,15 @@ const EventStackScreen = ({ navigation }) => {
 
 const AboutStackScreen = ({ navigation }) => {
     return (
-        <AboutStack.Navigator>
+        <AboutStack.Navigator screenOptions ={{
+            headerStyle:{
+                backgroundColor: '#009387',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold'
+            }
+        }}>
         <AboutStack.Screen name="AboutScreen" component={AboutScreen} />
     </AboutStack.Navigator>
     );
