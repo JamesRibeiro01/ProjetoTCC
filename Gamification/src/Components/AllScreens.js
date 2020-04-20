3ead26036b43922e5ecd3f46abea4bb28c173aa1
Thiago <thiagoworkspace@outlook.com>
2020-04-20 15:07:35 -0300

    deleted all

--- Gamification/src/Components/AllScreens.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import EventScreen from "./EventScreen";
-import NextEventScreen from "./NextEventScreen";
-import ActivityScreen from "./ActivityScreen";
-
-
-export {Home, EventScreen, NextEventScreen, ActivityScreen};
