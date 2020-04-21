b5ada9c67abeb0d3189341c74e19f64297a78225
Thiago <thiagoworkspace@outlook.com>
2020-04-21 20:37:11 -0300

    DrawerMenu

--- GamificationApp/index.js
+++ GamificationApp/index.js
@@ -2,8 +2,8 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, unstable_enableLogBox} from 'react-native';
 import Main from './src/Main';
 import {name as appName} from './app.json';
-
+unstable_enableLogBox();
 AppRegistry.registerComponent(appName, () => Main);
--- GamificationApp/src/Main.js
+++ GamificationApp/src/Main.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import MainTabScreen from "./Screens/MainTabScreen";
-
+import {DrawerContent}from "./Screens/DrawerContent";
 
 
 const Drawer = createDrawerNavigator();
@@ -12,7 +12,7 @@ const Drawer = createDrawerNavigator();
 const App = () => {
     return (
         <NavigationContainer>
-            <Drawer.Navigator initialRouteName = "Home">
+            <Drawer.Navigator  drawerContent = {props => <DrawerContent {...props} />}>
                 <Drawer.Screen name="Home" component={MainTabScreen} />
             </Drawer.Navigator>
         </NavigationContainer>
