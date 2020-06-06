817db60383df8c999f6a1600e94df9fb97506ffd
Thiago <thiagoworkspace@outlook.com>
2020-06-06 19:54:59 -0300

    tela HomeScreen

--- GamificationApp/src/Screens/HomeScreen.js
+++ GamificationApp/src/Screens/HomeScreen.js
@@ -1,76 +1,94 @@
-import React from "react";
-import { Text, View, StyleSheet, ScrollView } from "react-native";
-import Suggestions from "./SugestoesEventos/Suggestions";
-import Activitie from "./Activities/Activities";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
+export default class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
-export default function HomeScreen() {
-    return (
-
-        <View style={Wrapper.WrapperMain}>
-
-            <View style={Wrapper.WrapperHeader}>
-                <View style={Wrapper.WrapperBalanceContainer}>
-                    <Text style={Wrapper.WrapperBalanceTitle}>FAESA POINTS</Text>
+    render() {
+        return (
+            <View style={EstiloHome.ContainerView}>
+                <View style = {EstiloHome.containerText1}>
+                    <Text style = {EstiloHome.estiloTexto}> Usuario: Thiago Batista </Text>
                 </View>
-                <View style={Wrapper.WrapperBalanceTitle}>
-                    <Text style={Wrapper.WrapperBalance}>FP: 1000</Text>
+                <View style = {EstiloHome.containerText2}>
+                    <Text style = {EstiloHome.estiloTexto}> Matricula: 15110557 </Text>
                 </View>
+
+                <View style={EstiloHome.linha}></View>
+
+                <View style={EstiloHome.circle}>
+                    <Text style = {{backgroundColor: 'grey', 
+                                    color: 'white', 
+                                    textAlign: 'center'}}>
+                          Pontos: 1000
+                     </Text>
+                     <TouchableOpacity>
+                     <Text style = {{backgroundColor: 'grey', 
+                                    color: 'white', 
+                                    textAlign: 'center'}}>
+                          Aplicar Pontos
+                     </Text>
+                     </TouchableOpacity>
+                </View>
+
+                <View style={EstiloHome.linha2}></View>
             </View>
-            <View>
-                <Suggestions />
-                <View style={Estilos.containerView}>
-                    <View style={Estilos.containerView}>
-                    </View>
-                </View>
-            </View>
-
-        </View>
-
-    )
+        );
+    }
 }
 
 
-const Wrapper = StyleSheet.create({
-    WrapperMain: {
-        flex: 1
-    },
-
-    WrapperHeader: {
-        height: 50,
-        paddingVertical: 0,
-        paddingHorizontal: 16,
-        flexDirection: 'row',
+const EstiloHome = StyleSheet.create({
+    ContainerView: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        backgroundColor: '#003266'
     },
 
-    WrapperBalanceContainer: {
-
-    },
-
-    WrapperBalanceTitle: {
-        fontSize: 14,
-        textAlign: 'center',
-    },
-
-    WrapperBalance: {
-        textAlign: 'center',
-        fontSize: 20,
-        fontWeight: 'bold'
+    containerText1: {
+        paddingRight: 195,
+        marginTop: 20,
+        flexDirection: 'row'
     },
 
 
-});
-
-
-const Estilos = StyleSheet.create({
-    containerView:{
-
-
+    containerText2:{
+        paddingRight: 214,
+        marginTop: 9,
+        flexDirection: 'row'
     },
-    headerView:{
-        height: 50,
-        backgroundColor: 'red'
+
+    linha:{
+        marginTop: 10,
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 420,
+        backgroundColor: 'white'
+    },
+
+    linha2:{
+        marginTop: 200,
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 420,
+        backgroundColor: 'white'
+    },
+
+    circle:{
+        marginTop: 200,
+        marginHorizontal: 100,
+        borderWidth: 100,
+        borderRadius: 10,
+        width: 420,
+        borderColor: 'grey',
+        backgroundColor: 'white'
+    },
+
+    estiloTexto:{
+        color: 'white'
     }
 });
